ed3de791aae2c67cfed3fc424fde818ce033f51f
Maria Teilmann Andersen <j.yool@hotmail.com>
2021-07-16 23:57:57 +0200

    update list of connected clients upon disconnection

--- public/clients.js
+++ public/clients.js
@@ -3,6 +3,7 @@ const socket = io();
 
 // List of connected clients
 const listOfConnectedClients = document.getElementById("listOfConnectedClients");
+const listedDisconnectedClients = document.getElementById("listOfDisconnectedClients");
 
 socket.emit("joinedServer", {
     id: socket.id,
@@ -10,11 +11,38 @@ socket.emit("joinedServer", {
 });
 
 socket.on("clientsConnected", (data) => {
-    console.log(data);
+    console.log("connected users", data);
 
     data.forEach(element => {
         const client = document.createElement("li");
-        client.innerHTML = element.id;
-        listOfConnectedClients.appendChild(client);
+
+        if(element.id === socket.id) {
+            client.innerHTML = element.id + ' (You)';
+            listOfConnectedClients.appendChild(client);
+        } else {
+            client.innerHTML = element.id;
+            listOfConnectedClients.appendChild(client);
+        }
     });
 });
+
+socket.on("userDisconnect", (connectedUsers, disconnectedUsers) => {
+
+    // listOfConnectedClients.innerHTML = "";
+    // listedDisconnectedClients.innerHTML = "";
+
+    // connectedUsers.forEach(user => {
+    //     const client = document.createElement("li");
+    //     client.innerHTML = user.id;
+    //     listOfConnectedClients.appendChild(client);
+    // });
+
+    // disconnectedUsers.forEach(user => {
+    //     const client = document.createElement("li");
+    //     client.innerHTML = user.id;
+    //     listOfDisconnectedClients.appendChild(client);
+    // });
+    // console.log("connected users", connectedUsers);
+    console.log("Connected users: ", connectedUsers, ". Disconnected users: ", disconnectedUsers);
+});
+
--- server.js
+++ server.js
@@ -13,14 +13,39 @@ app.use(express.static("public"));
 // socket setup 
 const io = socket(server);
 
-// array containing connected clients
+// array containing connected and disconnected clients
 const connectedClients = [];
+const disconnectedClients = [];
 
 app.get("/", function (req, res) {
     res.sendFile(path.resolve("./public/clients.html"));
 })
 
 io.on("connection", (socket) => {
+
+    socket.on("disconnect", () => {
+
+        // firstly, remove disconnected client from list of connected users
+        let disconnectedUser;
+
+        for (let i = 0; i < connectedClients.length; i++) {
+            const user = connectedClients[i];
+            
+            if(user.id === socket.id) {
+                disconnectedUser = connectedClients.splice(i, 1);
+            }
+        }
+
+        // push disconnected user to list of disconnected clients
+        disconnectedClients.push(disconnectedUser);
+
+        // emit both lists to socket
+        socket.emit("userDisconnect", connectedClients, disconnectedClients);
+
+        console.log("disconnected user ", disconnectedUser);
+        console.log("disconnected clients", disconnectedClients); 
+        console.log("connected users", connectedClients);
+    });
 
     connectedClients.push({
         id: socket.id,
